eda190f0f517ee97cb396a07da9d2e1c819dcdd1
Julian Maurice <julian.maurice@biblibre.com>
2019-06-28 09:58:38 +0200

    Format code with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': [
+    'standard'
+  ],
+  'globals': {
+    'jQuery': 'readonly',
+    '_': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+  }
+}
